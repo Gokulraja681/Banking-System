12195e7896f2148c3d30cb6b4d2c15ad103e5b5f
Gokulraja681 <gokulrajabecse060801@gmail.com>
2024-01-19 11:05:33 +0530

    Successfully Venv is Created

---
+++ Venv/main.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>Hey Gokul Raja how are you ?</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
