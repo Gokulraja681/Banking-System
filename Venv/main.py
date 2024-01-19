3727bb3cead22c7abe4b6e4f57f246dc497ac00a
Gokulraja681 <gokulrajabecse060801@gmail.com>
2024-01-19 13:12:09 +0530

    Added

--- Venv/main.py
+++ Venv/main.py
@@ -5,5 +5,29 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/account_creation')
+def account_creation():
+    return render_template('account_creation.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/withdrawal')
+def withdrawal():
+    return render_template('withdrawal.html')
+
+@app.route('/deposit')
+def deposit():
+    return render_template('deposit.html')
+
+@app.route('/balance_enquiry')
+def balance_enquiry():
+    return render_template('balance_enquiry.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
