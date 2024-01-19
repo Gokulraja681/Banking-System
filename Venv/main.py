4ca13a80924be9f6e79a531ad89b94ea5ab28abc
Gokulraja681 <gokulrajabecse060801@gmail.com>
2024-01-19 17:29:55 +0530

    New Modifiaction are updates at 5.30 pm

--- Venv/main.py
+++ Venv/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,25 +9,76 @@ def home():
 def index():
     return render_template('index.html')
 
-@app.route('/account_creation')
+#Creating Some initiaters for the datas
+class GORA:
+    def __init__(self):
+        self.name = ""
+        self.father_name = ""
+        self.dob = ""
+        self.gender = ""
+        self.mbno = ""
+        self.pin = ""
+        self.amount = 0
+
+gr = GORA()
+
+# Account Creation Module
+@app.route('/account_creation', methods = ["POST", "GET"])
 def account_creation():
-    return render_template('account_creation.html')
+    msg = None
+    if request.method == "POST":
+        gr.name = request.form.get('name')
+        gr.father_name = request.form.get('father_name')
+        gr.dob = request.form.get('dob')
+        gr.gender = request.form.get('gender')
+        gr.mbno = request.form.get('mbno')
+        gr.pin = request.form.get('pin')
+        gr.amount = int(request.form.get('amount'))
+        msg = "Account has been created Successfully !"
+    return render_template('account_creation.html', msg = msg)
 
-@app.route('/profile')
-def profile():
-    return render_template('profile.html')
 
-@app.route('/withdrawal')
+# Account Holder's Profile Module
+@app.route('/profile', methods = ["GET"])
+def profile():   
+    return render_template('profile.html', name = gr.name, father_name = gr.father_name,
+                           dob = gr.dob, gender = gr.gender, mbno = gr.mbno,
+                           pin = gr.pin, amount = gr.amount)
+
+# Account Profile Module
+@app.route('/withdrawal', methods = ["POST", "GET"])
 def withdrawal():
-    return render_template('withdrawal.html')
+    amt = 0
+    total = 0
+    msg = None
+    if request.method == "POST":
+        amt  = int(request.form.get('amt'))
+        total = gr.amount - amt
+        gr.amount = total
+        msg = "Amount Withdrawal is Successful !"
+    return render_template('withdrawal.html', msg = msg, name = gr.name)
 
-@app.route('/deposit')
+# Amount Deposit Module
+@app.route('/deposit', methods = ["POST", "GET"])
 def deposit():
-    return render_template('deposit.html')
+    amt = 0
+    total = 0
+    msg = None
+    if request.method == "POST":
+        amt = int(request.form.get('amt'))
+        total = gr.amount + amt
+        gr.amount = total
+        msg = "Amount Deposited Successfully !"
+    return render_template('deposit.html', name = gr.name, msg = msg)
 
-@app.route('/balance_enquiry')
+# Balance Enquiry Module
+@app.route('/balance_enquiry', methods = ["GET", "POST"])
 def balance_enquiry():
-    return render_template('balance_enquiry.html')
+    msg = None
+    if request.method == "GET":
+        msg = "Your Account Current Balance Details"
+    return render_template('balance_enquiry.html', msg = msg, name = gr.name,
+                           amount = gr.amount)
 
 if __name__ == "__main__":
     app.run(debug=True)
