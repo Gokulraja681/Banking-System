49fc034274824ab139a886f364d95b4ce71dc5d0
Gokulraja681 <gokulrajabecse060801@gmail.com>
2024-02-11 18:53:44 +0530

    Database connected

--- Venv/main.py
+++ Venv/main.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import *
 import random
+import pymongo
+
 app = Flask(__name__)
+
+mongocon = pymongo.MongoClient('mongodb://localhost:27017')
+mydb = mongocon['Banking']
+mycol = mydb['Data']
+
+
 
 @app.route('/')
 def home():
@@ -30,31 +38,37 @@ def signup():
     if request.method == "POST":
         rg.fname = request.form.get('fname')
         rg.lname = request.form.get('lname')
-        mail = request.form.get('gmail')
-        if (mail == rg.gmail):
-            msg2 = "Account already exists"
+        temp_mail = request.form['gmail']
+        existing_user = mycol.find_one({'gmail': temp_mail})
+        if existing_user:
+            msg2 = 'Account already exists'
         else:
-            rg.gmail = mail
-        p = request.form.get('p')
-        if len(p) < 8:
-            msg = "Password must contain 8 characters"
-        rg.pswd = request.form.get('pswd')
-        if rg.pswd != p:
-            msg1 = "Password not matched"
-        if rg.pswd == p:
-            rg.pswd = p
-        if (len(p) >= 8) and (rg.pswd == p) and (mail != rg.gmail):
-            # notify = "User account has created successfully"
-            return redirect(url_for('signin'))
-    return render_template('signup.html', msg = msg, msg1 = msg1, 
-                           notify = notify, msg2 = msg2)
+            rg.gmail = temp_mail
+            p = request.form['p']
+            if len(p) < 8:
+                msg = 'Password must contain 8 characters'
+            else:
+                rg.pswd = request.form['pswd']
+                if rg.pswd != p:
+                    msg1 = 'password not matched with each other'
+                else:
+                    rg.pswd = p
+                    mydoc = {
+                        'fname': rg.fname,
+                        'lname': rg.lname,
+                        'gmail': rg.gmail,
+                        'pswd' : rg.pswd
+                    }
+                    mycol.insert_one(mydoc)
+                    return redirect(url_for('signin'))
+    return render_template('signup.html', msg=msg, msg1=msg1, notify=notify, msg2=msg2)
 
 # Account Profile module for testing
 @app.route('/details', methods = ["GET", "POST"])
 def details():
     if request.method == "GET":
-        return render_template('details.html', fname = rg.fname,
-                               lname = rg.lname, gmail = rg.gmail, pswd = rg.pswd)
+        all_details = mycol.find()
+        return render_template('details.html', all_details = all_details)
 
 # Signin Page module
 @app.route('/signin', methods = ["POST", "GET"])
@@ -64,10 +78,12 @@ def signin():
     if request.method == "POST":
         s_gmail = request.form.get('s_gmail')            
         s_pswd = request.form.get('s_pswd')
-        if (s_gmail != rg.gmail) or (s_pswd != rg.pswd):
-            msg = "Enter mail or password is not matched"
-        if (s_gmail == rg.gmail) and (s_pswd == rg.pswd):
+        user = mycol.find_one({'gmail': s_gmail})
+        if user and user['pswd'] == s_pswd:
+            session['user_data'] = user
             return redirect(url_for('index'))
+        else:
+            msg = "Enter mail or password is not matched"
     return render_template('signin.html', msg = msg, msg1 = msg1)
 
 # Forget Password Module
@@ -79,18 +95,23 @@ def forget_password():
     notify = None
     if request.method == "POST":
         f_gmail = request.form.get('f_gmail')
-        if (f_gmail) != (rg.gmail):
+        user = mycol.find_one({'gmail': f_gmail})
+        if user:
+            p = request.form['p']
+            if len(p) < 8:
+                msg1 = "Password must contain 8 character"
+            else:
+                f_pswd = request.form['f_pswd']
+            if (f_pswd != p):
+                msg2 = "Password not matched"
+            else:
+                rg.pswd = f_pswd
+                mycol.update_one({'gmail': f_gmail}, {'$set':{
+                    'pswd': f_pswd
+                }})
+                return redirect(url_for('signin'))
+        else:
             msg = "Please enter a valid mail-id"
-        p = request.form.get('p')
-        if len(p) < 8:
-            msg1 = "Password must contain 8 character"
-        f_pswd = request.form.get('f_pswd')
-        if (f_pswd != p):
-            msg2 = "Password not matched"
-        if (f_gmail == rg.gmail) and (len(p) >= 8) and (f_pswd == p):
-            rg.pswd = f_pswd
-            notify = "Password changed successfully !"
-            signin()
     return render_template('forget_password.html', msg = msg, msg1 = msg1, 
                            msg2 = msg2, notify = notify)
 
@@ -109,11 +130,13 @@ class GORA:
         self.gender = ""
         self.mbno = ""
         self.pin = ""
+        self.bmail = ""
         self.amount = 0
         self.acn = 0
 
 gr = GORA()
 
+# Account Creation Module
 # Account Creation Module
 @app.route('/account_creation', methods = ["POST", "GET"])
 def account_creation():
@@ -123,6 +146,7 @@ def account_creation():
     msg3 = None
     msg4 = None
     if request.method == "POST":
+        print(request.form)  # Add this line to check form data
         gr.name = request.form.get('name')
         gr.father_name = request.form.get('father_name')
         gr.dob = request.form.get('dob')
@@ -133,25 +157,42 @@ def account_creation():
         p = request.form.get('p')
         if len(p) != 6:
             msg = "Pin Should be in Six characters"
-        gr.pin = request.form.get('pin')
-        if gr.pin != p:
-            msg1 = "Pin not matched with each other"
-        gr.amount = int(request.form.get('amount'))
-        if gr.amount < 1000:
-            msg2 = "For account creation initial deposit amount is 1000"
-        if (len(p) == 6) and (gr.pin == p) and (gr.amount >= 1000):
-            msg3 = f"{gr.pin} is your account pin"
-            msg4 = "Account has been created successfully"
-    return render_template('account_creation.html', msg = msg, msg1 = msg1,
-                           msg2=msg2, msg3 = msg3, msg4 = msg4)
+        else:
+            gr.pin = request.form.get('pin')
+            if gr.pin != p:
+                msg1 = "Pin not matched with each other"
+            else:
+                gr.bmail = request.form['b_mail']  # Check here
+                user = mycol.find_one({'gmail': gr.bmail})
+                if user:
+                    gr.amount = int(request.form.get('amount'))
+                    if gr.amount < 1000:
+                        msg2 = "For account creation initial deposit amount is 1000"
+                    else:
+                        msg3 = f"{gr.pin} is your account pin"
+                        msg4 = "Account has been created successfully"
+                    mydoc={
+                        'Name': gr.name,
+                        'Father_Name': gr.father_name,
+                        'DOB': gr.dob,
+                        'Gender': gr.gender,
+                        'Mbno': gr.mbno,
+                        'Account_Number': gr.acn,
+                        'Pin': gr.pin,
+                        'Amount': gr.amount
+                    }
+                    mycol.update_one({'gmail': gr.bmail}, {'$set': mydoc})
+    return render_template('account_creation.html', msg=msg, msg1=msg1,
+                           msg2=msg2, msg3=msg3, msg4=msg4)
 
 
 # Account Holder's Profile Module
 @app.route('/profile', methods = ["GET"])
-def profile():   
-    return render_template('profile.html', acn = gr.acn ,name = gr.name, father_name = gr.father_name,
-                           dob = gr.dob, gender = gr.gender, mbno = gr.mbno,
-                           pin = gr.pin, amount = gr.amount)
+def profile():
+    if request.method == 'GET':
+        user_data = session.get('user_data')
+        return render_template('profile.html', user_data=user_data)
+
 
 # Account Profile Module
 @app.route('/withdrawal', methods = ["POST", "GET"])
